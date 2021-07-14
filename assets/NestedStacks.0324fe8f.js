0cd90f9636dc0091584cf7fdf091315ec97067bb
xinglei <253929992@qq.com>
2021-07-14 14:26:26 +0800

    add static

---
+++ assets/NestedStacks.0324fe8f.js
@@ -0,0 +1,1 @@
+import{o as e,c as n}from"./vendor.73f8751f.js";const r={};r.render=function(r,o,s,t,d,f){return e(),n("div",null,"version 2 不支持 nested stacks")};export default r;
---
+++ assets/Test.eca70625.js
@@ -0,0 +1,1 @@
+import{o as r,c as e}from"./vendor.73f8751f.js";const n={};n.render=function(n,o,t,d,f,s){return r(),e("div",null,"render UI")};export default n;
